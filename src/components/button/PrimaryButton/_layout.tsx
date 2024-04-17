0a81240aa7b880d8431f377f3f1f9dcf349a3c13
Engin Bolat <engi.bolat@gmail.com>
2024-04-17 23:00:44 +0300

    added isLoading action

--- src/components/button/PrimaryButton/_layout.tsx
+++ src/components/button/PrimaryButton/_layout.tsx
@@ -1,18 +1,21 @@
-import { StyleProp, Text, TextStyle, TouchableOpacity, ViewStyle } from "react-native";
+import { ActivityIndicator, StyleProp, Text, TextStyle, TouchableOpacity, ViewStyle } from "react-native";
 
 const PrimaryButton: React.FC<{
     title: string;
     onPress: () => void,
     style?: StyleProp<ViewStyle>;
     titleStyle?: StyleProp<TextStyle>,
+    isLoading?: boolean;
 }> = ({
     title,
     onPress,
     style,
     titleStyle,
+    isLoading = false,
 }) => {
         return <TouchableOpacity onPress={onPress} style={style} className={`w-full bg-black py-5 px-4 rounded-xl items-center justify-center`}>
-            <Text className="text-white" style={titleStyle}>{title}</Text>
+            {!isLoading && <Text className="text-white" style={titleStyle}>{title}</Text>}
+            {isLoading && <ActivityIndicator color={'white'} />}
         </TouchableOpacity>
     }
 
