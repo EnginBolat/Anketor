a571b30853a42dbc6df3c0a4012bee9149f39184
Engin Bolat <engi.bolat@gmail.com>
2024-04-17 22:29:08 +0300

    created home and index file

---
+++ src/pages/home/_layout.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Home = () => {
+    return <View className="flex flex-1 justify-center items-center">
+        <Text>Home</Text>
+    </View>
+}
+
+export default Home;
