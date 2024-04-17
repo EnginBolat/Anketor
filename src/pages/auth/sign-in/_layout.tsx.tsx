e843ac61f1e8054c2c0c7f4a58a2ff7dce2db4d0
Engin Bolat <engi.bolat@gmail.com>
2024-04-17 23:00:58 +0300

    added navigation for the success login

--- src/pages/auth/sign-in/_layout.tsx.tsx
+++ src/pages/auth/sign-in/_layout.tsx.tsx
@@ -2,13 +2,18 @@ import React, { useState } from "react";
 import { Alert, SafeAreaView, Text, TouchableOpacity, View } from "react-native";
 import * as Yup from 'yup';
 import { Formik } from "formik";
+import axios, { HttpStatusCode } from "axios";
 
 import { PrimaryButton, PrimaryInput } from "../../../components";
 import LocalStorage from "../../../core/service/local-storage/local.storage.service";
 import { LocalStorageSaveKeys } from "../../../constants";
+import { useNavigation } from "@react-navigation/native";
 
 const SignIn = () => {
+    const [loading, setLoading] = useState(false)
+    const [serviceError, setServiceErrorText] = useState('')
     const localStorage = new LocalStorage();
+    const navigation = useNavigation<any>();
 
     const SignupSchema = Yup.object().shape({
         nickname: Yup.string()
@@ -17,24 +22,47 @@ const SignIn = () => {
             .required('Required'),
     });
 
-
     async function handleSubmit(nickname: string, password: string) {
-        localStorage.save(LocalStorageSaveKeys.nickname, nickname);
-        localStorage.save(LocalStorageSaveKeys.password, password);
-        Alert.alert(`${nickname} ${password}`)
+        setLoading(true);
+        try {
+            axios.post('https://fakestoreapi.com/auth/login', {
+                username: nickname,
+                password: password,
+            })
+                .then(response => {
+                    switch (response.status) {
+                        case HttpStatusCode.Ok:
+                            localStorage.save(LocalStorageSaveKeys.nickname, nickname);
+                            localStorage.save(LocalStorageSaveKeys.password, password);
+                            console.log('Girdin Kral Hadi Bakim');
+                            navigation.navigate('Home')
+                            break;
+                    }
+                    setLoading(false);
+                })
+                .catch(error => {
+                    if (error.response && error.response.status === HttpStatusCode.Unauthorized) {
+                        setServiceErrorText('Kullanıcı adı veya parola hatalı.');
+                    } else {
+                        setServiceErrorText('Şu anda servislerimize bakım yapılıyor. Lütfen daha sonra tekrar deneyin.');
+                    }
+                    setLoading(false);
+                });
+        } catch (error) {
+            setServiceErrorText('Şu anda servislerimize bakım yapılıyor. Lütfen daha sonra tekrar deneyin.')
+        }
     }
 
     async function handleNavigationToSignIn() {
         Alert.alert(`Navigation...`)
     }
 
-
     return <SafeAreaView className="flex flex-1 p-5">
         <View className="flex flex-1 justify-center">
             <Text className="justify-start items-center font-semibold text-2xl mb-2 px-5">Hoşgeldiniz</Text>
             <Formik
                 validationSchema={SignupSchema}
-                initialValues={{ nickname: '', password: '', }}
+                initialValues={{ nickname: 'mor_2314', password: '83r5^_', }}
                 onSubmit={values => handleSubmit(values.nickname, values.password)}
             >
                 {({ handleChange, handleBlur, handleSubmit, values, errors, touched }) => (
@@ -44,6 +72,7 @@ const SignIn = () => {
                             setValue={handleChange('nickname')}
                             placeholder="Nickname"
                         />
+                        {errors.nickname && touched.nickname ? (<FormError error={errors.nickname} />) : null}
                         <PrimaryInput
                             value={values.password}
                             setValue={handleChange('password')}
@@ -54,8 +83,10 @@ const SignIn = () => {
                         <PrimaryButton
                             title="Giriş Yap"
                             onPress={handleSubmit}
+                            isLoading={loading}
                             titleStyle={{ fontWeight: '600' }}
                         />
+                        <FormError error={serviceError} />
                         <View className="w-full flex-row mt-4 justify-center items-center">
                             <Text>Üye Değil misiniz?</Text>
                             <TouchableOpacity onPress={handleNavigationToSignIn}>
