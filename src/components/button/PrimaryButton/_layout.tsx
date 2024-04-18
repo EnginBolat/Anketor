7f935324bdb1d3984c9d2b8150f5d61e33f46187
Engin Bolat <engi.bolat@gmail.com>
2024-04-18 09:21:24 +0300

    added leadingIcon property

--- src/components/button/PrimaryButton/_layout.tsx
+++ src/components/button/PrimaryButton/_layout.tsx
@@ -1,4 +1,5 @@
-import { ActivityIndicator, StyleProp, Text, TextStyle, TouchableOpacity, ViewStyle } from "react-native";
+import { ActivityIndicator, StyleProp, Text, TextStyle, TouchableOpacity, View, ViewStyle } from "react-native";
+import { SvgProps } from "react-native-svg";
 
 const PrimaryButton: React.FC<{
     title: string;
@@ -6,15 +7,21 @@ const PrimaryButton: React.FC<{
     style?: StyleProp<ViewStyle>;
     titleStyle?: StyleProp<TextStyle>,
     isLoading?: boolean;
+    isWhite?: boolean;
+    LeadingIcon?: any;
 }> = ({
     title,
     onPress,
     style,
     titleStyle,
     isLoading = false,
+    isWhite = false,
+    LeadingIcon,
 }) => {
-        return <TouchableOpacity onPress={onPress} style={style} className={`w-full bg-black py-5 px-4 rounded-xl items-center justify-center`}>
-            {!isLoading && <Text className="text-white" style={titleStyle}>{title}</Text>}
+        return <TouchableOpacity onPress={onPress} style={style} className={`w-full ${isWhite ? 'bg-white' : 'bg-black'} py-5 px-4 rounded-xl items-center justify-center`}>
+            {!isLoading && <View className="flex-row">
+                {LeadingIcon && <LeadingIcon />}
+                <Text className={`${!isWhite ? 'text-white' : 'text-black'}`} style={titleStyle}>{title}</Text></View>}
             {isLoading && <ActivityIndicator color={'white'} />}
         </TouchableOpacity>
     }
