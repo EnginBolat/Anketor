d029ae49456ce73fc019d3c8880eefecf8552e7a
Engin Bolat <engi.bolat@gmail.com>
2024-04-18 20:30:38 +0300

    new icons added

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,3 +1,7 @@
 export { default as IcEye } from './ic_eye.svg';
 export { default as IcFemale } from './ic_female.svg';
 export { default as IcMale } from './ic_male.svg';
+export { default as IcHome } from './ic_home.svg';
+export { default as IcUser } from './ic_user.svg';
+export { default as IcCalendar } from './ic_calendar.svg';
+export { default as IcClock } from './ic_clock.svg';
