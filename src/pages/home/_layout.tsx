4b75aabcde3213246bcf228dd7043f063eeeda7c
Engin Bolat <engi.bolat@gmail.com>
2024-04-19 19:29:13 +0300

    added filter for questions

--- src/pages/home/_layout.tsx
+++ src/pages/home/_layout.tsx
@@ -1,37 +1,66 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FlatList, SafeAreaView, Text, View } from "react-native";
 import { LocalStorage } from "../../core";
 import { LocalStorageSaveKeys } from "../../constants";
 import { AnketCard } from "../../components";
 import { useNavigation } from "@react-navigation/native";
+import questions from '../../utils/questions.json';
 
 const Home = () => {
     const localStorage = new LocalStorage();
     const nickname = localStorage.get(LocalStorageSaveKeys.nickname);
     const navigation = useNavigation<any>();
+    const [finishedQuestionsId, setFinishedQuestionsId] = useState<string[]>([]);
 
-    return <SafeAreaView className="flex flex-1 ">
-        <View className="flex flex-1 p-5 ">
-            <Text className="my-1 font-semibold text-xl">{`Hoşgeldin, ${nickname}`}</Text>
-            <Text className="my-1 font-semibold text-lg pt-5">{`Anketler`}</Text>
-            <FlatList
-                className="flex flex-1"
-                showsVerticalScrollIndicator={false}
-                scrollEnabled={true}
-                data={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
-                keyExtractor={(item, index) => index.toString()}
-                renderItem={(item) => {
-                    return <AnketCard
-                        title={`Anket ${item.index + 1}`}
-                        time="11:22"
-                        date="18.04.2024"
-                        onPress={() => { navigation.navigate('Question')}}
-                    />
-                }}
-            />
-        </View>
+    useEffect(() => {
+        const storedFinishedQuestionsId = localStorage.get(LocalStorageSaveKeys.finishedQuestionsId);
+        if (storedFinishedQuestionsId) {
+            setFinishedQuestionsId(storedFinishedQuestionsId);
+        }
+    }, []);
 
-    </SafeAreaView>
+    useEffect(() => {
+        navigation.addListener('focus', () => {
+            const storedFinishedQuestionsId = localStorage.get(LocalStorageSaveKeys.finishedQuestionsId);
+            if (storedFinishedQuestionsId) {
+                setFinishedQuestionsId(storedFinishedQuestionsId);
+            }
+        });
+    }, [navigation]);
+
+    const filterQuestionsById = (questions: any) => {
+        return questions.filter((question: any) => {
+            for (const id of finishedQuestionsId) {
+                if (question.id === id) {
+                    console.log(question.id);
+                    return false;
+                }
+            }
+            return true;
+        });
+    };
+
+    return (
+        <SafeAreaView className="flex flex-1 ">
+            <View className="flex flex-1 p-5 ">
+                <Text className="my-1 font-semibold text-xl">{`Hoşgeldin ${nickname ? `,${nickname}` : ""}`}</Text>
+                <Text className="my-1 font-semibold text-lg pt-5">{`Anketler`}</Text>
+                <FlatList
+                    className="flex flex-1"
+                    showsVerticalScrollIndicator={false}
+                    scrollEnabled={true}
+                    data={finishedQuestionsId ? filterQuestionsById(questions) : questions}
+                    keyExtractor={(item, index) => index.toString()}
+                    renderItem={({ item }) => (
+                        <AnketCard
+                            title={item.title}
+                            onPress={() => { navigation.navigate('Question', { testId: item.id }) }}
+                        />
+                    )}
+                />
+            </View>
+        </SafeAreaView>
+    );
 }
 
 export default Home;
