ca9899d4bbae50c7ca81f9752b67869dcec1a03f
Engin Bolat <engi.bolat@gmail.com>
2024-04-19 19:28:59 +0300

    MultipleChoiceQuestionSaveModel renamed as SaveAnswersModel

--- src/model/index.ts
+++ src/model/index.ts
@@ -1,4 +1,4 @@
-export type { default as MultipleChoiceQuestionSaveModel } from './multiple.choice.question.save.model';
+export type { default as SaveAnswersModel } from './save.answers.model';
 export type { default as MultipleChoiceQuestionModel } from './multiple.choice.question.model';
 export type { default as OpenEndedQuestionModel } from './open.ended.question';
 export type { default as ScaleQuestionModel } from './scale.question.model';
