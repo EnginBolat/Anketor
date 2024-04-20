2a359901ebedc6dfda2f86c7357526045f910208
Engin Bolat <engi.bolat@gmail.com>
2024-04-20 19:16:33 +0300

    added title and averagepoint

--- src/model/save.answers.model.ts
+++ src/model/save.answers.model.ts
@@ -1,9 +1,11 @@
 interface SaveAnswersModel {
     questionId: number;
+    title: string;
     answers: string;
     totalTime: string;
     createdDate: string;
     updatedDate: string;
+    averagePoint: string;
 }
 
 export default SaveAnswersModel;
