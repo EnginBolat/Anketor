fdb217bb460d5ce7c7a89f2cf33a254f304c168d
Engin Bolat <engi.bolat@gmail.com>
2024-04-21 19:42:58 +0300

    added scrollview

--- src/pages/auth/_layout.tsx
+++ src/pages/auth/_layout.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Text, View } from "react-native";
+import { Image, Text, View, SafeAreaView, ScrollView } from "react-native";
 import { SignIn } from "./sign-in";
 import { SignUp } from "./sign-up";
 import { SegmentedControl } from "../../components";
@@ -8,18 +8,29 @@ const Auth = () => {
     const [selectedOption, setselectedOption] = useState('Giriş Yap');
     const [isSignIn, setIsSignIn] = useState(true);
 
-    return <View className="flex flex-1 justify-center items-center">
-        <Text className="justify-start items-center font-semibold text-2xl my-5 px-5">Hoşgeldiniz</Text>
-        <SegmentedControl
-            options={['Giriş Yap', 'Kayıt Ol',]}
-            selectedOption={selectedOption}
-            onOptionPress={(value: any) => {
-                setselectedOption(value);
-                value === "Kayıt Ol" ? setIsSignIn(false) : setIsSignIn(true);
-            }} />
-        <View className="py-1" />
-        {isSignIn ? <SignIn /> : <SignUp />}
-    </View>
+    return (
+        <SafeAreaView className="flex flex-1 justify-center items-center bg-white">
+            <ScrollView
+                showsHorizontalScrollIndicator={false}
+                showsVerticalScrollIndicator={false}
+                style={{ width: "100%" }}
+                contentContainerStyle={{ alignItems: 'center' }}>
+                <Text className="justify-start items-center font-semibold text-2xl my-5 px-5">Hoşgeldiniz</Text>
+                <SegmentedControl
+                    options={['Giriş Yap', 'Kayıt Ol',]}
+                    selectedOption={selectedOption}
+                    onOptionPress={(value: any) => {
+                        setselectedOption(value);
+                        value === "Kayıt Ol" ? setIsSignIn(false) : setIsSignIn(true);
+                    }} />
+                {selectedOption === "Kayıt Ol"
+                    ? <Image source={require("../../assets/images/img_3d_label.png")} style={{ height: 192, width: 192, marginVertical: 6 }} />
+                    : <Image source={require("../../assets/images/img_3d_world.png")} style={{ height: 192, width: 192, marginVertical: 6 }} />}
+                <View className="py-1" />
+                {isSignIn ? <SignIn /> : <SignUp />}
+            </ScrollView>
+        </SafeAreaView>
+    );
 }
 
 export default Auth;
